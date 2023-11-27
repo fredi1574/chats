1c06b51a9fddc095a44ad4bd2e89cd8f18a55907
Fredi Bulshtein <Fredi1574@gmail.com>
2023-11-27 20:55:10 +0200

    changed the file to support firestore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Import the necessary libraries
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+//TODO: add android support
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDj8H1PI0a6Bu4QMvOj-7md9qXCbLwXk64",
   authDomain: "chats-366af.firebaseapp.com",
@@ -13,9 +12,10 @@ const firebaseConfig = {
   storageBucket: "chats-366af.appspot.com",
   messagingSenderId: "164919283277",
   appId: "1:164919283277:web:d5f53c98eefb37446e9604",
-  measurementId: "G-6GKQCTTL28",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the database
+export const db = getFirestore(app);
